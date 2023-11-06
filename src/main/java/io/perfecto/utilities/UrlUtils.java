b34eafaf7880b3898feb11d874d1a98420173f92
hristo.genev <hristo.genev@gmail.com>
2023-11-06 12:50:58 +0200

    Changed getUrl to return string

--- src/main/java/io/perfecto/utilities/UrlUtils.java
+++ src/main/java/io/perfecto/utilities/UrlUtils.java
@@ -7,11 +7,10 @@ public class UrlUtils {
     private static String perfectoServerAddress = "https://%s.perfectomobile.com/nexperience/perfectomobile/wd/hub/";
     private static String localServerAddress = "http://127.0.0.1:4723/wd/hub/";
 
-    public static URL getUrl(String cloudName) throws MalformedURLException {
+    public static String getUrl(String cloudName) throws MalformedURLException {
 
         boolean isLocal = cloudName.equals("local");
-        String urlString = isLocal ? localServerAddress : String.format(perfectoServerAddress, cloudName);
-        return new URL(urlString);
+        return isLocal ? localServerAddress : String.format(perfectoServerAddress, cloudName);
     }
 }
 

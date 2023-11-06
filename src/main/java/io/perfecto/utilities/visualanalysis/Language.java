303b8bd8b6dcd11921bb5d3e0202a88c99c79711
hristo.genev <hristo.genev@gmail.com>
2023-11-06 12:50:38 +0200

    Added all supported VA languages

--- src/main/java/io/perfecto/utilities/visualanalysis/Language.java
+++ src/main/java/io/perfecto/utilities/visualanalysis/Language.java
@@ -2,4 +2,14 @@ package io.perfecto.utilities.visualanalysis;
 
 public interface Language {
     String ENGLISH = "English";
+    String GERMAN = "German";
+    String CHINESE = "ChinesePRC";
+    String DUTCH = "Dutch";
+    String FRENCH = "French";
+    String ITALIAN = "Italian";
+    String JAPANESE = "Japanese";
+    String KOREAN = "Korean";
+    String PORTUGUESE = "PortugueseStandard";
+    String RUSSIAN = "Russian";
+    String SPANISH = "Spanish";
 }

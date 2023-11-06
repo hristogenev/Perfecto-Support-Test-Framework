6e86111da05805a165fd1b5f8bfbf1c86615e6f3
hristo.genev <hristo.genev@gmail.com>
2023-11-06 12:50:25 +0200

    Added default value when getting a property

--- src/main/java/io/perfecto/utilities/CommonProperties.java
+++ src/main/java/io/perfecto/utilities/CommonProperties.java
@@ -7,6 +7,23 @@ import java.util.Properties;
 public class CommonProperties {
 
     private static Properties properties;
+
+    public static String getProperty(String propertyName, Object defaultValue) {
+
+        if (properties != null)
+            return properties.getProperty(propertyName, (String)defaultValue);
+
+        try (InputStream input = new FileInputStream(CommonProperties.class.getClassLoader().getResource("common.properties").getPath())) {
+            properties = new Properties();
+            properties.load(input);
+            return properties.getProperty(propertyName);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return (String)defaultValue;
+        }
+    }
+
     public static String getProperty(String propertyName) {
 
         if (properties != null)
